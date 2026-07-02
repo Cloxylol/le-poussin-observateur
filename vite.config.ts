a86df552517a4c2ef1c6f7df8cad5cbfabd021a5
Cloe <cloe.petetin@gmail.com>
2026-07-02 21:21:51 +0200

    Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/le-poussin-observateur/',
   plugins: [
     react(),
     tailwindcss(),
