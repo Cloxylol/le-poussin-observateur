f272c42b0c0e38daafa302a1620327ee7ac164d4
Cloe <cloe.petetin@gmail.com>
2026-07-02 21:22:00 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/le-poussin-observateur/sw.js', { scope: '/le-poussin-observateur/' })})}
